d618195315f92f4488fb5145d5e9df5b661581ba
LySivty <sivtyly9@gmail.com>
2025-07-01 22:19:39 +0800

    import product to amazon.js

--- script/amazon.js
+++ script/amazon.js
@@ -1,4 +1,5 @@
 import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 // because amazon.js in the scripte folder to make it get the file outside that folder need to use " .. "
 
 let productsHTML = "";
