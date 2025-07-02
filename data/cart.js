e0c63026ecf23b68645a81814db22838b4256384
LySivty <sivtyly9@gmail.com>
2025-07-02 18:30:57 +0800

    Generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,19 @@
-export const cart = [];
+import { products } from "../data/products.js";
+
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
 
 export function addTocart(productId) {
   const quantitySelect = document.querySelector(
-    `.js-quantity-selector-${productId}` //DOM can't use product.id so we use productId
+    `.js-quantity-selector-${productId}`
   );
 
   const quantity = Number(quantitySelect.value); //this how to convert string to Number
